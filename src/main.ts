ec38ede869361e16ba7947af086548149e0e1415
@Draichi <lucasdraichi@gmail.com>
2022-09-07 02:53:08 -0300

    feat: add material contact

--- src/main.ts
+++ src/main.ts
@@ -43,11 +43,24 @@ const environmentMapTexture = cubeTextureLoader.load([
 const world = new CANNON.World();
 world.gravity.set(0, -9.82, 0);
 
+const concreteMaterial = new CANNON.Material("concrete");
+const plasticMaterial = new CANNON.Material("plastic");
+const concreteAndPlasticContactMaterial = new CANNON.ContactMaterial(
+  concreteMaterial,
+  plasticMaterial,
+  {
+    friction: 0.1,
+    restitution: 0.7,
+  }
+);
+world.addContactMaterial(concreteAndPlasticContactMaterial);
+
 const sphereShape = new CANNON.Sphere(0.5);
 const sphereBody = new CANNON.Body({
   mass: 1,
   position: new CANNON.Vec3(0, 3, 0),
   shape: sphereShape,
+  material: plasticMaterial,
 });
 world.addBody(sphereBody);
 
@@ -55,6 +68,7 @@ const floorShape = new CANNON.Plane();
 const floorBody = new CANNON.Body({
   mass: 0,
   shape: floorShape,
+  material: concreteMaterial,
 });
 floorBody.quaternion.setFromAxisAngle(new CANNON.Vec3(1, 0, 0), -Math.PI * 0.5);
 world.addBody(floorBody);
